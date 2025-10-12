c63bb4f1f7e5db4897b300333203905f37ec8ef7
MayquelVR <mvargas11rubio@gmail.com>
2025-10-12 00:54:36 -0500

    add liquibase

---
+++ src/main/java/com/viewdatatools/apigenarator/config/LiquibaseConfig.java
@@ -0,0 +1,28 @@
+package com.viewdatatools.apigenarator.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfig {
+
+    @Value("${spring.liquibase.change-log}")
+    private String changeLog;
+
+    @Value("${spring.liquibase.default-schema}")
+    private String defaultSchema;
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog(changeLog);
+        liquibase.setDefaultSchema(defaultSchema);
+        liquibase.setShouldRun(true);
+        return liquibase;
+    }
+}
